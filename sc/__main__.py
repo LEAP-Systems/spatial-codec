baf129b71ad448b643d6ae2c4485767adccb03a2
cSDes1gn <christian911@sympatico.ca>
2020-08-03 16:26:21 -0400

    for visualizing targets

--- sc/__main__.py
+++ sc/__main__.py
@@ -1,4 +1,8 @@
 
 from sc.scodec import SpatialCodec
 
-SpatialCodec(10,10)
+inputs = ((10,3), (5,4), (2,6), (4,6), (1,9))
+
+sc = SpatialCodec(10,10)
+sc.visualizer.populate(inputs)
+sc.visualizer.render()
