3801aae9218d7a8613c1d9d8aa7d8f490ad4d624
cSDes1gn <christian911@sympatico.ca>
2020-08-03 16:27:00 -0400

    adding populate()

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 class Visualizer:
 
     def __init__(self, frame:tuple):
-        plt.figure(figsize=frame)
-        plt.axis('equal')
-        # plt.fill(x, y)
+        plt.figure(figsize=(8,8))
+        rx, ry = frame
+        plt.axis([0, rx, 0, ry])
+        
+    def render(self):
         plt.show()
+    
+    def populate(self, targets:set):
+        plt.scatter(
+            list(map( lambda x : x[0], targets)),
+            list(map( lambda x : x[1], targets)),
+        )
