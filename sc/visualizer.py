8562864d5a5dd8562773ec13a8b28c2b381e4b46
cSDes1gn <christian911@sympatico.ca>
2020-08-03 21:24:47 -0400

    small changes

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -6,10 +6,14 @@ class Visualizer:
 
     def __init__(self, frame:tuple):
         plt.figure(figsize=(8,8))
-        rx, ry = frame
-        plt.axis([0, rx, 0, ry])
+        self.rx, self.ry = frame
+        plt.axis([0, self.rx, 0, self.ry])
+        # draw dividers
+        plt.plot([self.rx/2, self.rx/2], [0,self.ry],linewidth=1, linestyle='dashed', color='red')
+        plt.plot([0, self.rx], [self.ry/2,self.ry/2],linewidth=1, linestyle='dashed', color='red')
         
     def render(self):
+        self.line()
         plt.show()
     
     def populate(self, targets:set):
@@ -17,3 +21,10 @@ class Visualizer:
             list(map( lambda x : x[0], targets)),
             list(map( lambda x : x[1], targets)),
         )
+
+    def line(self):
+        # creating 4 equivalent sectors
+        offset = self.rx/2
+        
+        # draw curve
+        # plt.plot()
