7f92a77a8daf159ee74ce212f330a9b18118f4e2
cSDes1gn <christian911@sympatico.ca>
2020-08-04 23:32:40 -0400

    testing dim 256

--- sc/__main__.py
+++ sc/__main__.py
@@ -3,6 +3,6 @@ from sc.scodec import SpatialCodec
 
 # inputs = ((10,3), (5,4), (2,6), (4,6), (1,9))
 
-sc = SpatialCodec(128,128)
+sc = SpatialCodec(256,256)
 # sc.visualizer.populate(inputs)
 sc.visualizer.render()
