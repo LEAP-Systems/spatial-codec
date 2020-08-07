f4c310b3d8284247a1ae7ee7539dbfa5a8fbb2b8
cSDes1gn <christian911@sympatico.ca>
2020-08-06 23:46:33 -0400

    support env vars

--- sc/__main__.py
+++ sc/__main__.py
@@ -1,8 +1,11 @@
-
+import os
 from sc.scodec import SpatialCodec
 
 # inputs = ((10,3), (5,4), (2,6), (4,6), (1,9))
 
-sc = SpatialCodec(256,256)
+if os.environ['SC_ENV'] == 'dev':
+    sc = SpatialCodec(2,2, dev=True,optimize=False)
+else:
+    sc = SpatialCodec(2,2, dev=False, optimize=True)
 # sc.visualizer.populate(inputs)
 sc.visualizer.render()
