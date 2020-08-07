b08e943624e48f13a8745ef635f7190f922be544
cSDes1gn <christian911@sympatico.ca>
2020-08-07 00:13:29 -0400

    execute logging config on init

---
+++ sc/__init__.py
@@ -0,0 +1,11 @@
+"""
+Spatial Codec __init___
+===================================
+
+Logger initialization for spatial codec
+
+Copyright © 2020 LEAP. All Rights Reserved.
+"""
+
+# execute logger config
+from sc.logs import config
