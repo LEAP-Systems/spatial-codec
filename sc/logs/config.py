ff29f1abcdaddafadcf88836bb7797774cfa16b1
cSDes1gn <christian911@sympatico.ca>
2020-08-07 00:15:53 -0400

    logging script

---
+++ sc/logs/config.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Spatial Codec Logging Configuration
+===================================
+
+Copyright © 2020 Christian Sargusingh. All Rights Reserved.
+"""
+import logging.config
+import os
+from pathlib import Path
+
+import yaml
+
+# cleanup all previous logs for new runtime environment
+cwd = os.getcwd()
+os.chdir(Path(__file__).parent.parent)
+
+logs = list(
+    filter(lambda file : os.path.isfile(file) and file.split(".")[1] == "log", os.listdir())
+)
+for log in logs:
+    os.remove(log)
+os.chdir(cwd)
+
+CONFIG_PATH = Path(__file__).parent.joinpath("config.yaml")
+
+# check for existance of config.yaml
+if not CONFIG_PATH.exists():
+    raise FileNotFoundError
+
+# configure the logger
+with open(CONFIG_PATH) as file:
+    logging.config.dictConfig(yaml.full_load(file))
