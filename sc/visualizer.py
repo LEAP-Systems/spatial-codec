66b8c0d85d0b32636d7e3256ba9efe99b0fe9485
cSDes1gn <christian911@sympatico.ca>
2021-05-22 23:30:08 -0400

    add 3d plot function

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -1,34 +1,42 @@
 
 
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 class Visualizer:
 
-    def __init__(self, frame:tuple):
-        self.rx, self.ry = frame
-        # plt.axis([0, self.rx-1, 0, self.ry-1])
-        # draw dividers
-        # plt.plot([self.rx/2, self.rx/2], [0,self.ry],linewidth=1, linestyle='dashed', color='red')
-        # plt.plot([0, self.rx], [self.ry/2,self.ry/2],linewidth=1, linestyle='dashed', color='red')
-        # plt.plot([0, self.rx], [0,self.ry],linewidth=1, color='red')
-        
-    def render(self):
+    @staticmethod
+    def plot_3d(data_set, decoder):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        xs, ys, zs, = list(), list(), list()
+        for coor, neuron in data_set.items():
+            label = f"({neuron.x},{neuron.y})"
+            ax.text(coor[0],coor[1],coor[2], label, size=6, zorder=1, color='k') 
+            xs.append(coor[0])
+            ys.append(coor[1])
+            zs.append(coor[2])
+        ax.scatter(xs, ys, zs, marker='o')
+        hcx, hcy, hcz, = list(), list(), list()
+        # plot hilberts curve
+        for coor in decoder.hc:
+            hcx.append(coor[0])
+            hcy.append(coor[1])
+            hcz.append(coor[2])
+        plt.plot(hcx, hcy, hcz, marker='o')
+        # plt.plot(hcx,hcy,hcz)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+    
+    @staticmethod        
+    def render() -> None:
         plt.axis('off')
         plt.grid(b=False)
         plt.show()
-    
-    # def populate(self, targets:set):
-    #     plt.scatter(
-    #         list(map( lambda x : x[0], targets)),
-    #         list(map( lambda x : x[1], targets)),
-    #     )
 
-    def line(self, d:list):
-        # creating 4 equivalent sectors
-        # offset = self.rx/2
-        # draw curve
-        # plt.plot()
+    @staticmethod
+    def line(d:list) -> None:
         x = list(map(lambda x : x[0], d))
         y = list(map(lambda x : x[1], d))
         plt.plot(x, y, linewidth=1, color='black')
-        # plt.scatter(x,y)
