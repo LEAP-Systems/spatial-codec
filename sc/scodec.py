f3ee42cadd093fc649c2cb055afbbf1d8b1e7c50
cSDes1gn <christian911@sympatico.ca>
2021-05-23 00:16:59 -0400

    map 2d model in 3d

--- sc/scodec.py
+++ sc/scodec.py
@@ -33,6 +33,7 @@ class SpatialCodec:
         self.log.info("s vector: %s", self.s)
         # inputs = [self.encode(x) for x in range(self.res)]
         inputs = [self.encode3d(x) for x in range(self.res)]
+        self.visualizer.plot_3d(inputs)
         # self.visualizer.line(inputs)
 
     def decode(self, n:int, x:int, y:int) -> int:
@@ -63,22 +64,22 @@ class SpatialCodec:
             # Once the index reaches 0 the x and y bits are latched and alternate between each other
             # before converging before we exceed the range boundary n
             if i == 0:
-                # we are done
+                # optimization for starting case
                 if x == y: break
-                # compute last flip (i required for forcasting)
-                x,y = (y,x) if (self.res-index) & 1 else (x,y)
+                # compute last flip (if odd flip if even keep)
+                x,y,z = (y,x,z) if (self.res-index) & 1 else (x,y,z) 
                 break
             # parity checks on last bits
-            rx = 1 & (i >> 1) # is index/2 odd?
-            ry = 1 & (i ^ rx) # is index/2 odd and index odd?
-                              # is index/2 even and index even?
+            rx = 1 & (i >> 1) # is i/2 odd?
+            ry = 1 & (i ^ rx) # is i/2 odd and i odd?
+                              # is i/2 even and i even?
             if ry == 0:
-                if rx == 1: x,y = s-1 - x, s-1 - y
-                x,y = y,x
+                if rx == 1: x,y,z = s-1 - x, s-1 - y, z
+                x,y,z = y,x,z
             x += s * rx
             y += s * ry
-            i = i >> 2
-            self.log.debug("i:%s s:%s | rx:%s ry:%s | x:%s y:%s z:%s", i, s, rx, ry, x, y, z)
+            i = i >> 2 # log2i (reverse of s vector)
+            # self.log.debug("i:%s s:%s | rx:%s ry:%s rz:%s | x:%s y:%s z:%s", i, s, rx, ry, rz, x, y, z)
         self.log.info("resolved i:%s -> x:%s y:%s z:%s", i, x,y,z)
         return x,y,z
 
