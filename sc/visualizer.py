272a64ff26ef80f49f4301e60d8cae78ca1e6173
cSDes1gn <christian911@sympatico.ca>
2021-05-23 01:14:36 -0400

    remove render method

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -18,12 +18,6 @@ class Visualizer:
         ax.set_zlabel('Z')
         plt.show()
     
-    @staticmethod        
-    def render() -> None:
-        plt.axis('off')
-        plt.grid(b=False)
-        plt.show()
-
     @staticmethod
     def line(d:list) -> None:
         x = list(map(lambda x : x[0], d))
@@ -33,3 +27,4 @@ class Visualizer:
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         plt.plot(x, y, linewidth=1, color='black')
+        plt.show()
