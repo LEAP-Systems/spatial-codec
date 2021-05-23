0b28a6864a7cd7610938c859915f9c7e5ab6c47f
cSDes1gn <christian911@sympatico.ca>
2021-05-23 00:17:09 -0400

    update 3d plotting fn

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -5,25 +5,16 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 class Visualizer:
 
     @staticmethod
-    def plot_3d(data_set, decoder):
+    def plot_3d(coordinates:tuple) -> None:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        xs, ys, zs, = list(), list(), list()
-        for coor, neuron in data_set.items():
-            label = f"({neuron.x},{neuron.y})"
-            ax.text(coor[0],coor[1],coor[2], label, size=6, zorder=1, color='k') 
-            xs.append(coor[0])
-            ys.append(coor[1])
-            zs.append(coor[2])
-        ax.scatter(xs, ys, zs, marker='o')
         hcx, hcy, hcz, = list(), list(), list()
         # plot hilberts curve
-        for coor in decoder.hc:
-            hcx.append(coor[0])
-            hcy.append(coor[1])
-            hcz.append(coor[2])
+        for x,y,z in coordinates:
+            hcx.append(x)
+            hcy.append(y)
+            hcz.append(z)
         plt.plot(hcx, hcy, hcz, marker='o')
-        # plt.plot(hcx,hcy,hcz)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
