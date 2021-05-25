0de3b33498519f9d2034c199f6c92436dcd9befb
cSDes1gn <christian911@sympatico.ca>
2021-05-24 23:57:56 -0400

    scoping abc abstracts

--- sc/scodec.py
+++ sc/scodec.py
@@ -11,36 +11,38 @@ Dependancies
 Copyright © 2020 Christian Sargusingh
 """
 
-from typing import Tuple
+import logging
+from typing import List, Tuple
 from abc import ABC, abstractmethod
 from sc.visualizer import Visualizer
 
 class SpatialCodec(ABC):
 
-    def __init__(self, resolution:int, dim:int) -> None:
+    def __init__(self, resolution:int) -> None:
+        self.log = logging.getLogger(__name__)
         self.resolution = resolution
-        self.dim = dim
         self.s = [2**x for x in range(self.resolution)]
+        self.log.info("s vector: %s", self.s)
         self.visualizer = Visualizer()
         super().__init__()
 
     @abstractmethod
-    def stream_encode(self): ...
+    def stream_encode(self, bytestream:bytes) -> None:...
 
     @abstractmethod
-    def stream_decode(self): ...
+    def stream_decode(self, coor:List[Tuple]) -> bytes: ...
+
+    @abstractmethod
+    def decode(self) -> int: ...
+    
+    @abstractmethod
+    def encode(sefl): ...
+
+    @abstractmethod
+    def render(self, coors:List[Tuple]): ...
     
     @abstractmethod
     def transform(self): ...
 
     @abstractmethod
     def iterator(self, i:int) -> Tuple: ...
-    
-    @abstractmethod
-    def decoder(self): ...
-    
-    @abstractmethod
-    def encoder(sefl): ...
-
-    @abstractmethod
-    def render(self): ...
