67abae5e0166c6898c3bc6effdb2f19f74d930c7
cSDes1gn <christian911@sympatico.ca>
2021-05-24 23:57:12 -0400

    split algorithm into child classes on dimension selection

--- sc/__main__.py
+++ sc/__main__.py
@@ -1,9 +1,13 @@
+from sc.n3 import N3
+from sc.n2 import N2
 import sys
 import getopt
 import logging
 from sc.scodec import SpatialCodec
 
 def main(argv) -> None:
+    dimension = 0
+    resolution = 0
     try:
         opts, _ = getopt.getopt(argv, "r:d:", ["resolution=", "dimension="])
     except getopt.GetoptError:
@@ -14,7 +18,14 @@ def main(argv) -> None:
             resolution = int(arg)
         elif opt in ("-d", "--dimension"):
             dimension = int(arg)
-    SpatialCodec(resolution, dimension)
+    # N2/N3 impl split
+    if dimension == 2:
+        sc = N2(resolution=resolution)
+    elif dimension == 3:
+        sc = N3(resolution=resolution)
+    else:
+        raise ValueError("Spatial codec algorithm defined for 2D and 3D space filling curves")
+    sc.stream_encode(b'\xffffffff')
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
