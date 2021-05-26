d605a79318e0424aa59c4277274544ec50bea4ad
cSDes1gn <christian911@sympatico.ca>
2021-05-26 01:19:17 -0400

    disable autoscaling and preconfigure range limits

--- sc/visualizer.py
+++ sc/visualizer.py
@@ -1,5 +1,4 @@
 
-
 from typing import List, Tuple
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
@@ -7,25 +6,43 @@ class Visualizer:
 
     @staticmethod
     def plot_3d(d:List[Tuple[int,int,int]]) -> None:
+        plt.style.use('dark_background')
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         x = list(map(lambda x : x[0], d))
         y = list(map(lambda x : x[1], d))
         z = list(map(lambda x : x[2], d))
+        # Set the limits of the plot
+        cube_max = max([max(x), max(y), max(z)])
+        plt.xlim(0, cube_max)
+        plt.ylim(0, cube_max)
+        # Don't mess with the limits!
+        plt.autoscale(False)
         # plot hilberts curve
         plt.plot(x, y, z, marker='o')
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
+        ax.set_zlim(0,cube_max)
         plt.show()
     
     @staticmethod
-    def line(d:List[Tuple]) -> None:
-        x = list(map(lambda x : x[0], d))
-        y = list(map(lambda x : x[1], d))
+    def d2(coor:List[Tuple], base:List[Tuple[int,int]]) -> None:
+        plt.style.use('dark_background')
+        x = list(map(lambda x : x[0], base))
+        y = list(map(lambda x : x[1], base))
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
-        plt.plot(x, y, linewidth=1, color='black')
+        # Set the limits of the plot
+        sq_max = max(y) if max(y) > max(x) else max(x)
+        plt.xlim(0, sq_max)
+        plt.ylim(0, sq_max)
+        # Don't mess with the limits!
+        plt.autoscale(False)
+        plt.plot(x, y, linewidth=3, color='black')
+        x = list(map(lambda x : x[0], coor))
+        y = list(map(lambda x : x[1], coor))
+        plt.plot(x, y, marker='o', color='red') 
         plt.show()
