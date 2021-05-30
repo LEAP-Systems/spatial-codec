c84e885fd2ce2974d06db8aa118335852a6f3e30
cSDes1gn <christian911@sympatico.ca>
2021-05-30 14:39:00 -0400

    stashing changes

--- sc/n3.py
+++ sc/n3.py
@@ -21,7 +21,15 @@ class N3(SpatialCodec):
         # spatial codec init
         super().__init__(resolution=resolution)
 
-    def stream_encode(self, bytestream:bytes) -> None: ...
+    def stream_encode(self, bytestream:bytes) -> None:
+        # index = [self.encode(i) for i in range(self.resolution)]
+        for curve in [(('x','y','z'),'r'),(('x','z','y'),'b'),(('y','x','z'),'g'),(('y','z','x'),'y'),(('z','x','y'),'c'),(('z','y','x'),'m')]:
+            order, clr = curve
+            index = [self.iterator(i,order) for i in range(self.resolution)]
+            # self.render(index)
+            self.visualizer.add_curve(index, str(order),clr)
+            self.log.info("Added curve %s with label %s", index, str(order))
+        self.visualizer.show()
 
     def stream_decode(self, coor:List[Tuple[int,int,int]]) -> bytes: ...
 
@@ -43,6 +51,7 @@ class N3(SpatialCodec):
     def encode(self, i:int) -> Tuple[int,int,int]:
         # initial coordinates
         x,y,z = 0,0,0
+        r_x,r_y,r_z = 0,0,0
         for index,s in enumerate(self.s):
             self.log.debug("starting iteration %s",index)
             # Once the index reaches 0 the x and y bits are latched and alternate between each other
@@ -60,7 +69,6 @@ class N3(SpatialCodec):
             x += s * r_x
             y += s * r_y
             z += s * r_z
-            
             self.log.info("i:%s s:%s | rx:%s ry:%s rz:%s | x:%s y:%s z:%s", i, s, r_x, r_y, r_z, x, y, z)
         self.log.info("resolved i:%s -> x:%s y:%s z:%s", i, x,y,z)
         return x,y,z
@@ -88,14 +96,14 @@ class N3(SpatialCodec):
         #     else:
         #         if r_x == 1: x,y,z = y,z,x
         #         else: x,y,z = x,y,z
-        if r_z == 1:
+        if r_z == 0:
             if r_y == 0:
-                if r_x == 1: x,y,z = s-1-x,s-1-y,s-1-z
+                if r_x == 1: x,y,z = x,s-1-y,s-1-z
                 x,y,z = y,x,z
             x,y,z = z,x,y
         return x,y,z
 
-    def iterator(self, i:int) -> Tuple[int,int,int]:
+    def iterator(self, i:int, o:Tuple[str,str,str]) -> Tuple[int,int,int]:
         """
         Base iterator for N3 algorithm    
 
@@ -104,12 +112,27 @@ class N3(SpatialCodec):
         :return: base cartesian coordinate of bit @ index i
         :rtype: Tuple[int,int,int]
         """
+        # x,y,z
+        # x,z,y
+        # y,x,z
+        # y,z,x
+        # z,y,x
+        # z,x,y
         r_x = 1 & (i >> 2)
         r_y = 1 & (i >> 1 ^ r_x)
         r_z = 1 & (i ^ r_x ^ r_y)
         # regions of seperation (8 verticies)
-        i = i >> 3 
-        return r_x, r_y, r_z
+        i = i >> 3
+        if o[0] == 'x':
+            if o[1] == 'y': r_t = r_x, r_y, r_z
+            else: r_t = r_x, r_z, r_y
+        elif o[0] == 'y':
+            if o[1] == 'x': r_t = r_y, r_x, r_z
+            else: r_t = r_y, r_z, r_x
+        else:
+            if o[1] == 'x': r_t = r_z, r_x, r_y
+            else: r_t = r_z, r_y, r_x 
+        return r_t
 
     def render(self, coors: List[Tuple[int,int,int]]) -> None:
         self.visualizer.plot_3d(coors)
