9f23af966741d65824238356683606826697e3ac
cSDes1gn <christian911@sympatico.ca>
2021-05-31 23:18:12 -0400

    rework n3 to use single iterations

--- sc/n3.py
+++ sc/n3.py
@@ -5,98 +5,51 @@ N3 Spatial Codec
 Contributors: Christian Sargusingh
 Updated: 2021-05
 
-https://www.desmos.com/calculator/hpzbeqkqc1
 
 Dependancies
 ------------
 
 Copyright © 2020 Christian Sargusingh
 """
+
 from sc.scodec import SpatialCodec
 from typing import List, Tuple
-from sc.visualizer import Visualizer
+from sc.iterators import Iterators
 
 class N3(SpatialCodec):
     def __init__(self, resolution:int):
+        if resolution > 8:
+            raise NotImplementedError('This version supports single iteration (max 8 bit resolution) curves in 3D space')
         # spatial codec init
         super().__init__(resolution=resolution)
 
     def stream_encode(self, bytestream:bytes) -> None:
-        variations = [
-            (('x','y','z'),'k'),
-            (('x','-y','z'),'r'),
-            (('x','y','-z'),'b'),
-            (('x','z','y'),'g'),
-            (('x','-z','y'),'y'),
-            (('x','z','-y'),'c'),
-            (('x','-y','-z'),'m'),
-            (('x','-z','-y'),'blueviolet'),
-            # (('-x','y','z'),'k'),
-            # (('-x','-y','z'),'r'),
-            # (('-x','y','-z'),'b'),
-            # (('-x','z','y'),'g'),
-            # (('-x','-z','y'),'y'),
-            # (('-x','z','-y'),'c'),
-            # (('-x','-y','-z'),'m'),
-            # (('-x','-z','-y'),'blueviolet'),
-            (('y','x','z'),'k'),
-            # (('y','-x','z'),'r'),
-            (('y','x','-z'),'b'),
-            (('y','z','x'),'g'),
-            (('y','-z','x'),'y'),
-            # (('y','z','-x'),'c'),
-            # (('y','-x','-z'),'m'),
-            # (('y','-z','-x'),'blueviolet'),
-            (('-y','x','z'),'k'),
-            # (('-y','-x','z'),'r'),
-            (('-y','x','-z'),'b'),
-            (('-y','z','x'),'g'),
-            (('-y','-z','x'),'y'),
-            # (('-y','z','-x'),'c'),
-            # (('-y','-x','-z'),'m'),
-            # (('-y','-z','-x'),'blueviolet'),
-            (('z','x','y'),'k'),
-            # (('z','-x','y'),'r'),
-            (('z','x','-y'),'b'),
-            (('z','y','x'),'g'),
-            (('z','-y','x'),'y'),
-            # (('z','y','-x'),'c'),
-            # (('z','-x','-y'),'m'),
-            # (('z','-y','-x'),'blueviolet'),
-            (('-z','x','y'),'k'),
-            # (('-z','-x','y'),'r'),
-            (('-z','x','-y'),'b'),
-            (('-z','y','x'),'g'),
-            (('-z','-y','x'),'y'),
-            # (('-z','y','-x'),'c'),
-            # (('-z','-x','-y'),'m'),
-            # (('-z','-y','-x'),'blueviolet'),
-        ]
-        self.log.info(len(variations))
-        # variation iterator
-        for curve in variations:
-            order, clr = curve
-            # bit iterator
-            li = []
-            for i in range(self.resolution):
-                x,y,z = 0,0,0
-                # region iterator
-                for index,s in enumerate(self.s):
-                    r_x,r_y,r_z = self.iterator(i,order)
-                    i = i >> 3
-                    x += s * r_x # x = x + (s | 0)
-                    y += s * r_y # y = y + (s | 0)
-                    z += s * r_z # z = z + (s | 0)
-                li.append((x,y,z))
-            # self.render(index)
-            self.visualizer.add_curve(li, str(order),clr)
-            self.log.info("Added curve %s with label %s", li, str(order))
-        self.visualizer.show()
-        # index = [self.encode(i) for i in range(self.resolution)]
-        # self.log.info(index)
-        # self.render(index)
+        # remove excess bytes if word exceeds an 8 bit number
+        bitstream = int(bytestream.hex(), base=16) & 0xff
+        self.log.debug("bitstream: %s", bin(bitstream))
+        # stretch bistream into iterable of single bits
+        bits = [bitstream >> i for i in range(start=1,stop=9)]
+        # generate index by encoding each set bit sequentially
+        index = list(filter(None, [self.encode(i) if b else None for i,b in enumerate(bits)]))
+        self.log.info(index)
+        base = [self.encode(i) for i,_ in enumerate(bits)]
+        self.render(index, base)
 
     def stream_decode(self, coor:List[Tuple[int,int,int]]) -> bytes: ...
+
+    def show_iterators(self) -> None:
+        """
+        Interactive visualizer for iteration variations
+        """
+        # variation iterator
+        for curve in Iterators.variations:
+            order, clr = curve
+            self.visualizer.add_curve(
+                d=[self.transform(*self.iterator(i), order) for i in range(8)],
+                label=str(order),
+                clr=clr
+            )
+        self.visualizer.show()
 
     def decode(self, n:int, x:int, y:int) -> int:
         d = 0
@@ -116,28 +69,43 @@ class N3(SpatialCodec):
     def encode(self, i:int) -> Tuple[int,int,int]:
         # initial coordinates
         x,y,z = 0,0,0
-        for index,s in enumerate(self.s):
-            self.log.debug("starting iteration %s",index)
-            # Once the index reaches 0 the x and y bits are latched and alternate between each other
-            # before converging before we exceed the range boundary n
-            if i == 0:
-                # optimization for starting case, flipping does not do anything
-                if x == y == z: break
-            #     # compute last flip (if odd flip if even keep)
-            #     x,y,z = (y,x,z) if (self.res-index) & 1 else (x,y,z) 
-            #     break
-            # compute region selector bits
-            # coordinates offsets by region
-            # the iterator changes with different i values
-            r_x,r_y,r_z = self.iterator(i, ('x','z','y'))
-            # regions of seperation (8 verticies)
-            i = i >> 3
-            x,y,z = self.transform(r_x,r_y,r_z,x,y,z,s)
-            self.log.info("i:%s s:%s | rx:%s ry:%s rz:%s | x:%s y:%s z:%s", i, s, r_x, r_y, r_z, x, y, z)
+        # for index,s in enumerate(self.s):
+        #     self.log.debug("starting iteration %s",index)
+        #     # Once the index reaches 0 the x and y bits are latched and alternate between each other
+        #     # before converging before we exceed the range boundary n
+        #     if i == 0:
+        #         # optimization for starting case, flipping does not do anything
+        #         if x == y == z: break
+        #         # compute last flip (if odd flip if even keep)
+        #         x,y,z = (y,x,z) if (self.res-index) & 1 else (x,y,z) 
+        #         break
+        #     # compute region selector bits
+        #     # coordinates offsets by region
+        #     # the iterator changes with different i values
+        #     r_x,r_y,r_z = self.iterator(i, ('x','z','y'))
+        #     # regions of seperation (8 verticies)
+        #     i = i >> 3
+        #     x,y,z = self.transform(r_x,r_y,r_z,x,y,z,s)
+        #     self.log.info("i:%s s:%s | rx:%s ry:%s rz:%s | x:%s y:%s z:%s", i, s, r_x, r_y, r_z, x, y, z)
+        self.iterator(i)
         self.log.info("resolved i:%s -> x:%s y:%s z:%s", i, x,y,z)
         return x,y,z
 
-    def transform(self, r_x:int,r_y:int,r_z:int, x:int, y:int, z:int, s:int) -> Tuple[int,int,int]:
+    def iterator(self, i:int) -> Tuple[int,int,int]:
+        """
+        Base iterator for N3 algorithm
+
+        :param i: probe index
+        :type i: int
+        :return: base cartesian coordinate of bit @ index i
+        :rtype: Tuple[int,int,int]
+        """
+        r_x = 1 & (i >> 2)
+        r_y = 1 & (i >> 1 ^ r_x)
+        r_z = 1 & (i ^ r_x ^ r_y)
+        return r_x,r_y,r_z
+
+    def transform(self, r_x:int, r_y:int, r_z:int, o:Tuple[str,str,str]) -> Tuple[int,int,int]:
         # region selection and transform application
         # lambda x,y,z: (x + (s * r_x), y + (s * r_y), z + (s * r_z))
         # if (r_x,r_y,r_z) == (0,0,0):
@@ -150,26 +118,12 @@ class N3(SpatialCodec):
         #     x,y,z = -z,y,x
         # else:
         #     x,y,z = -z,x,y
+        # x += s * r_x # x = x + (s | 0)
+        # y += s * r_y # y = y + (s | 0)
+        # z += s * r_z # z = z + (s | 0)
         # translate base iterator
-        x += s * r_x # x = x + (s | 0)
-        y += s * r_y # y = y + (s | 0)
-        z += s * r_z # z = z + (s | 0)
-        return x,y,z
-
-    def iterator(self, i:int, o:Tuple[str,str,str]) -> Tuple[int,int,int]:
-        """
-        Base iterator for N3 algorithm
-
-        :param i: probe index
-        :type i: int
-        :return: base cartesian coordinate of bit @ index i
-        :rtype: Tuple[int,int,int]
-        """
-        r_t = 0,0,0
-        r_x = 1 & (i >> 2)
-        r_y = 1 & (i >> 1 ^ r_x)
-        r_z = 1 & (i ^ r_x ^ r_y)
         # iterator variant selector (-x and x are somehow the same?)
+        r_t = r_x, r_y, r_z
         if o[0] == 'x':
             if o[1] == 'y':
                 if o[2] == 'z': r_t = r_x, r_y, r_z
@@ -248,7 +202,10 @@ class N3(SpatialCodec):
             else:
                 if o[2] == 'x': r_t = 1-r_z, 1-r_y, r_x
                 else: r_t = 1-r_z, 1-r_y, 1-r_x
-        return r_t
+        return r_t 
 
-    def render(self, coors: List[Tuple[int,int,int]]) -> None:
-        self.visualizer.plot_3d(coors)
+    def render(self, index:List[Tuple[int,int,int]], base:List[Tuple[int,int,int]]) -> None:
+        self.log.debug("index: %s", index)
+        self.log.debug("base: %s", base)
+        self.visualizer.add_curve(index, label='index', clr='r')
+        self.visualizer.add_curve(base, label='base', clr='k')
