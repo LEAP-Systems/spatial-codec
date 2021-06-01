e56724e83170f70d39dc45037b3304a8e6beef5c
cSDes1gn <christian911@sympatico.ca>
2021-06-01 00:58:37 -0400

    module docstring for abc

--- sc/scodec.py
+++ sc/scodec.py
@@ -3,11 +3,16 @@
 Spatial Codec™ ABC
 ==================
 
+Abstract base class implemented by n2 and n3 spatial codec algorithms
+
 Dependancies
 ------------
 ```
+import logging
+from typing import List, Tuple
+from abc import ABC, abstractmethod
+from sc.visualizer import Visualizer
 ```
-
 Copyright © 2020 Christian Sargusingh
 """
 
@@ -15,6 +20,7 @@ import logging
 from typing import List, Tuple
 from abc import ABC, abstractmethod
 from sc.visualizer import Visualizer
+
 
 class SpatialCodec(ABC):
 
@@ -27,20 +33,20 @@ class SpatialCodec(ABC):
         super().__init__()
 
     @abstractmethod
-    def stream_encode(self, bytestream:bytes) -> None:...
+    def stream_encode(self, bytestream:bytes) -> None: ...
 
     @abstractmethod
     def stream_decode(self, coor:List[Tuple]) -> bytes: ...
 
     @abstractmethod
     def decode(self) -> int: ...
-    
+
     @abstractmethod
     def encode(self): ...
 
     @abstractmethod
     def render(self, coors:List[Tuple]): ...
-    
+
     @abstractmethod
     def transform(self): ...
 
