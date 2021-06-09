8c5742d95f6e61f5eb432bdf900583461a8c4efc
cSDes1gn <christian911@sympatico.ca>
2021-06-09 00:36:05 -0400

    update cli args

--- sc/__main__.py
+++ sc/__main__.py
@@ -8,36 +8,39 @@ from sc.n2 import N2
 
 def main(argv) -> None:
     # defaults
+    be = "utf-8"
     dimension = 0
-    resolution = 0
-    input_stream = bytes("default", "utf-8")
+    block = 0
+    input_stream = bytes("default", be)
     mpl = False
     # parse opts
     try:
-        opts, _ = getopt.getopt(argv, "s:d:v:", ["string=", "dimension=", "verbose="])
+        opts, _ = getopt.getopt(
+            argv, "n:b:d:v:", ["dimension=", "block=", "data=", "verbose="])
     except getopt.GetoptError:
-        logging.exception("python -m sc -s $STRING -d $DIMENSION -v=")
+        logging.exception("python -m sc -n 2 -b 32 -s test -v=")
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-s", "--string"):
-            input_stream = bytes(arg, "utf-8")
-            # compute bit resolution of stream
-            resolution = len(input_stream) * 8
-        elif opt in ("-d", "--dimension"):
+        if opt in ("-d", "--data"):
+            input_stream = bytes(arg, be)
+        elif opt in ("-n", "--dimension"):
             dimension = int(arg)
         elif opt in ("-v, --verbose"):
             mpl = True
+        elif opt in ("-b, --block"):
+            block = int(arg)
     logging.info("Input stream: %s", input_stream)
-    logging.info("Bit resolution: %s", resolution)
+    logging.info("Byte Encoding: %s", be)
+    logging.info("Block size: %s", block)
     logging.info("Encoding dimension: %s", dimension)
     logging.info("MPL Visualizer: %s", mpl)
     # N2/N3 impl split
     if dimension == 2:
-        n2_sc = N2(resolution)
+        n2_sc = N2(block)
         encode_stream = n2_sc.stream_encode(input_stream, mpl=mpl)
         bytestream = n2_sc.stream_decode(encode_stream, len(input_stream))
     elif dimension == 3:
-        n3_sc = N3(resolution)
+        n3_sc = N3(block)
         encode_stream = n3_sc.stream_encode(input_stream, mpl=mpl)
         bytestream = n3_sc.stream_decode(encode_stream, len(input_stream))
     else:
