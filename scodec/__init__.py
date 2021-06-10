dd6f12a5e80b92bf1d1f80ba643cd29bc06e4124
cSDes1gn <christian911@sympatico.ca>
2021-06-09 23:17:23 -0400

    load logging config directly from log directory

--- scodec/__init__.py
+++ scodec/__init__.py
@@ -7,8 +7,23 @@ Initialization for spatial codec
 Copyright © 2021 LEAP. All Rights Reserved.
 """
 
-from scodec.logs.config import config
+import yaml
+from pathlib import Path
+import logging
+import logging.config
+
 from scodec.__version__ import __version__
 
-# execute logger config
-config()
+
+# cleanup all previous logs for new runtime environment
+CONFIG_PATH = Path(__file__).parent.joinpath("config/log.yaml")
+
+# check for existance of config.yaml
+if not CONFIG_PATH.exists(): raise FileNotFoundError
+
+# configure the logger
+with open(CONFIG_PATH) as file:
+    logging.config.dictConfig(yaml.full_load(file))
+
+_log = logging.getLogger(__name__)
+_log.info("scodec version: %s", __version__)
